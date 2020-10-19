0863ab68015dc4a5361d416526ca04a2c7f4d4a7
William <willspecker@gmail.com>
2020-10-19 15:04:07 +0200

    Adding tests to test whether new created contacts appear in application and adding div for contact list and connecting app.js to html file

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -10,5 +10,14 @@ describe('user can create a contact', () => {
         cy.get('#company').type('Craft Academy')
         cy.get('#notes').type('Student coder')
         cy.get('#twitter').type('@alasihavenotwitter')
+        cy.get('#submit').click()
+       })
+
+       it('displays the name of a new contact', () => {
+           cy.get('#contact-list').should('contain', 'William')
+       })
+
+       it('displays the phone number of the new contact', () => {
+           cy.get('#contact-list').should('contain', '0702 550482')
        })
 })
