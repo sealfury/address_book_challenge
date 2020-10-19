cc1bc7b250dfab7221f88e27157839c144caac3e
William <willspecker@gmail.com>
2020-10-19 14:53:28 +0200

    Adding cypress code to test whether a user can add Name, Email, Phone nr, Company, Notes about the conact, Twitter, and adding corresponding code to html file to allow user to enter the above

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -4,5 +4,11 @@ describe('user can create a contact', () => {
     it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
-    })
+        cy.get('#name').type('William')
+        cy.get('#email').type('willspecker@gmail.com')
+        cy.get('#phone').type('0702 550482')
+        cy.get('#company').type('Craft Academy')
+        cy.get('#notes').type('Student coder')
+        cy.get('#twitter').type('@alasihavenotwitter')
+       })
 })
